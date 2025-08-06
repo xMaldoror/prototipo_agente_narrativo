ea1f6de86d396af3b5be4ac38f2dba391a6c7747
Francisco Pereira <francisco.pereira993@gmail.com>
2025-08-06 10:55:35 +0100

    9th
    O agente apresenta bons resultados

--- iniciar_agente_narrativo.bat
+++ iniciar_agente_narrativo.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d "%~dp0"
-echo Iniciando o Agente Narrativo com Ética Emergente...
+echo Iniciando o Agente Narrativo com Etica Emergente...
 call venv\Scripts\activate.bat
 python main.py
 pause
